eaa4b8c8d3096bba43a2fadbe651b8389b899b78
KMR <66520073+kmr77@users.noreply.github.com>
2020-11-14 07:21:06 +0900

    Initial Commit
    初回コミット

---
+++ VueJs/js/main.js
@@ -0,0 +1,12 @@
+(function(){
+    'use strict';
+
+    var vm = new Vue({
+        el: "#app",
+        data: {
+            name: ""
+        }
+    }); 
+})();
+
+//https://github.com/kmr77/vuejs.git
